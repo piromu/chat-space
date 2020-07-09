c78bacdc9f0b938f8c9a358ea1c8a53de9bcc3d0
piromu <pdfrrh47@gmail.com>
2020-07-10 05:00:56 +0900

    AsynchronousTransmission

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,68 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Messagebox">
+          <div class="Messageinfo">
+            <div class="Messageinfo__username">
+              ${message.user_name}
+            </div>
+            <div class="Messageinfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Messagebox">
+        <div class="Messageinfo">
+          <div class="Messageinfo__username">
+            ${message.user_name}
+          </div>
+          <div class="Messageinfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Messagespace').append(html);      
+      $('form')[0].reset();
+      $('.Messagespace').animate({ scrollTop: $('.Messagespace')[0].scrollHeight});
+    })
+    .always(function(data){
+      $('.Form__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
